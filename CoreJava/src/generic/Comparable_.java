c93c521a6a654723ee10e9025b3a143c86a59e67
NayanChaturved <chaturvedinayan5>
2023-09-12 16:35:37 +0530

    4

--- CoreJava/src/generic/Comparable_.java
+++ CoreJava/src/generic/Comparable_.java
@@ -32,13 +32,13 @@ class Employee1 implements Comparable<Employee1>
 
 	public int compareTo(Employee1 emp)
 	{
-		if(emp.age > this.age)
+		if(this.age> emp.age)
 		{
 			return 1;
 		}
 		else
 		{
-			return 1;
+			return -1;
 		}
 	}
 }
@@ -49,10 +49,15 @@ public class Comparable_
 	{
 		Employee1 emp1=new Employee1(21, "Nayan");
 		Employee1 emp2=new Employee1(34, "Vijay");
+		Employee1 emp3=new Employee1(4, "Anoop");
+
 		
 		List<Employee1> al=new ArrayList<>();
 		al.add(emp1);
 		al.add(emp2);
+		al.add(emp3);
+		
+		Collections.sort(al);
 		
 		System.out.println(al);
 
---
+++ YouTube/src/rough1/Rougj_1.java
@@ -0,0 +1,10 @@
+package rough1;
+
+public class Rougj_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
