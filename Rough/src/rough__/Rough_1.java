fb9f405f73dc6ba734cd829a7760a789a7710181
NayanChaturved <chaturvedinayan5>
2023-11-11 13:36:47 +0530

    2nd

--- Rough/src/rough__/Rough_1.java
+++ Rough/src/rough__/Rough_1.java
@@ -1,67 +1,31 @@
 package rough__;
 
+class Std<T>
+{
+	T obj;
+	
+	Std(T obj)
+	{
+		this.obj=obj;
+	}
+	
+	public void prop()
+	{
+		System.out.println(obj.getClass().getName());
+	}
+	
+	public T getObj()
+	{
+		return obj;
+	}
+}
+
 public class Rough_1
 {
-	public static void main(String[] args)
+	public static void main(String[] args) 
 	{
-		int num=10;
-		
-		for(int i=0; i<num; i++)
-		{
-			for(int j=0; j<num; j++)
-			{
-				if(j==0 || i==j || i==num-1)
-				{
-					System.out.print("*");
-				}
-				else
-				{
-					System.out.print(" ");
-				}
-			}
-			
-			
-			for(int j=0; j<num; j++)
-			{
-				if(i+j==num|| j==num-1 || i==num-1 )
-				{
-					System.out.print("*");
-				}
-				else
-				{
-					System.out.print(" ");
-				}
-			}
-			System.out.println();
-		}
-		
-		for(int i=0; i<num; i++)
-		{
-			for(int j=0; j<num; j++)
-			{
-				if(i==0 || j==0 || i+j==num)
-				{
-					System.out.print("*");
-				}
-				else
-				{
-					System.out.print(" ");
-				}
-			}
-			
-			for(int j=0; j<num; j++)
-			{
-				if(i==0 || j==num-1 || i==j)
-				{
-					System.out.print("*");
-				}
-				else
-				{
-					System.out.print(" ");
-				}
-			}
-			System.out.println();
-		}
-			
+		Std<String> s=new Std<String>("Nayan");
+		s.prop();
+		System.out.println(s.obj);
 	}
 }
